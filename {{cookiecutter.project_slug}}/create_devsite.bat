054eb98d0ffd2725018581bad05dae15b3eb7203
SINTES Jeremy <jeremy.sintes@soprasteria.com>
2018-04-20 12:34:59 +0200

    moving the tools... in the toolbox

--- {{cookiecutter.project_slug}}/create_devsite.bat
+++
@@ -1,3 +0,0 @@
-cookiecutter --config-file toolbox/devsite/devsite_cookiecutter_config.yaml https://github.com/idea-to-the-moon/-2-create_devsite.git
-
-:end
---
+++ {{cookiecutter.project_slug}}/toolbox/create_devsite.bat
@@ -0,0 +1,3 @@
+cookiecutter --config-file devsite/devsite_cookiecutter_config.yaml https://github.com/idea-to-the-moon/-2-create_devsite.git
+
+:end
